7cb57f2e0583cc605805f2edfcd93d375bc3a03f
Nazar Vinnichuk <nazar.vinnichuk@tutanota.com>
2020-07-27 14:45:55 +0300

    Integrate somehow cairo and md4c.

--- src/main.c
+++ src/main.c
@@ -1,7 +1,12 @@
-#include <md4c.h>
+#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <md4c.h>
 #include <cairo/cairo.h>
 #include <cairo/cairo-pdf.h>
+
+cairo_surface_t *surface;
+cairo_t *cr;
 
 int enter_block(MD_BLOCKTYPE bt, void *d, void *ud) {
     return 0;
@@ -20,7 +25,10 @@ int leave_span(MD_SPANTYPE st, void *d, void *ud) {
 }
 
 int text(MD_TEXTTYPE tt, const MD_CHAR *t, MD_SIZE s, void *ud) {
-    fwrite(t, 1, s, stdout);
+    char *buf = malloc(s + 1);
+    strncpy(buf, t, s);
+    buf[s] = '\0';
+    cairo_show_text (cr, buf);
     return 0;
 }
 
@@ -37,19 +45,16 @@ int main() {
         NULL
     };
 
-    MD_CHAR *data = "Blah *Italics* Blah *BOLD*                ";
-    md_parse(data, 30, &parser, NULL);
-
-    cairo_surface_t *surface =
-        cairo_pdf_surface_create ("cairo.pdf", 240, 80);
-    cairo_t *cr =
-        cairo_create (surface);
+    surface = cairo_pdf_surface_create ("cairo.pdf", 500, 500);
+    cr = cairo_create (surface);
 
     cairo_select_font_face (cr, "serif", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_BOLD);
     cairo_set_font_size (cr, 32.0);
     cairo_set_source_rgb (cr, 0.0, 0.0, 1.0);
     cairo_move_to (cr, 10.0, 50.0);
-    cairo_show_text (cr, "Hello, world");
+
+    MD_CHAR *data = "Blah *Italics* Blah *BOLD*                ";
+    md_parse(data, 30, &parser, NULL);
 
     cairo_destroy (cr);
     cairo_surface_destroy (surface);
