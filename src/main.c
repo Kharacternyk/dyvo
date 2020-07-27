d74692692944baf0b1e30ef488bf76d99095caef
Nazar Vinnichuk <nazar.vinnichuk@tutanota.com>
2020-07-27 21:39:40 +0300

    Handle italics and bold.

--- src/main.c
+++ src/main.c
@@ -17,10 +17,25 @@ int leave_block(MD_BLOCKTYPE bt, void *d, void *ud) {
 }
 
 int enter_span(MD_SPANTYPE st, void *d, void *ud) {
+    switch (st) {
+    case MD_SPAN_EM:
+        cairo_select_font_face(cr, "serif",
+                               CAIRO_FONT_SLANT_ITALIC, CAIRO_FONT_WEIGHT_NORMAL);
+        break;
+    case MD_SPAN_STRONG:
+        cairo_select_font_face(cr, "serif",
+                               CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_BOLD);
+        break;
+    default:
+        cairo_select_font_face(cr, "serif",
+                               CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_BOLD);
+        break;
+    }
     return 0;
 }
 
 int leave_span(MD_SPANTYPE st, void *d, void *ud) {
+    cairo_select_font_face(cr, "serif", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_BOLD);
     return 0;
 }
 
@@ -28,7 +43,11 @@ int text(MD_TEXTTYPE tt, const MD_CHAR *t, MD_SIZE s, void *ud) {
     char *buf = malloc(s + 1);
     strncpy(buf, t, s);
     buf[s] = '\0';
+
+    cairo_move_to(cr, 10.0, 50.0);
     cairo_show_text(cr, buf);
+    cairo_show_page(cr);
+
     free(buf);
     return 0;
 }
@@ -52,10 +71,9 @@ int main() {
     cairo_select_font_face(cr, "serif", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_BOLD);
     cairo_set_font_size(cr, 32.0);
     cairo_set_source_rgb(cr, 0.0, 0.0, 1.0);
-    cairo_move_to(cr, 10.0, 50.0);
 
-    MD_CHAR *data = "Blah *Italics* Blah *BOLD*                ";
-    md_parse(data, 30, &parser, NULL);
+    MD_CHAR *data = "Some normal text. *Italics* Blah **BOLD**                ";
+    md_parse(data, 45, &parser, NULL);
 
     cairo_destroy(cr);
     cairo_surface_destroy(surface);
