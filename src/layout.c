3c6274df166b4c95a2f78c667fddc5fec4b323ac
Nazar Vinnichuk <nazar.vinnichuk@tutanota.com>
2020-08-10 15:51:57 +0300

    Get rid of recursive layout_t.

--- src/layout.c
+++ src/layout.c
@@ -9,22 +9,13 @@
 #include "display.h"
 #include "util.h"
 
-#define PAGE_MARGIN 10
-
-typedef struct layout_t {
-    double x1, y1, x2, y2;
-    struct layout_t *next;
-} layout_t;
-
-static layout_t def1 = {
-    PAGE_MARGIN, PAGE_MARGIN,
-    PAGE_WIDTH - PAGE_MARGIN, PAGE_HEIGHT - PAGE_MARGIN,
-    NULL
-};
+#define PAGE_DEF_MARGIN 10
 
 static void layout_file(cairo_t *cr, const char *filename) {
     char *data = fread_till_end(filename);
-    display_pango_markup(cr, data, def1.x1, def1.y1, def1.x2, def1.y2);
+    display_pango_markup(cr, data,
+                         PAGE_DEF_MARGIN, PAGE_DEF_MARGIN,
+                         PAGE_WIDTH - PAGE_DEF_MARGIN, PAGE_HEIGHT - PAGE_DEF_MARGIN);
     free(data);
 }
 
